250710b34a49d1836858047a9d12ace8ff98919e
Adouken133 <adouken1101@gmail.com>
2019-10-31 20:27:42 -0400

    my dataset

---
+++ myproject.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+wine = pd.read_csv(filepath_or_buffer='wine.data',header=0)
+to_wine = wine.to_string()
+wine_DS = wine.describe()
+wine_INFO = wine.info()
+wine_cor = wine.corr()
+
+#os.makedirs('plots')
+# 1 Column by line chart
+plt.plot(wine['Alcohol'], color='blue')
+plt.title('Alcohol by Index')
+plt.xlabel('Index')
+plt.ylabel('Alcohol')
+plt.savefig(f'plots/Alcohol_by_index_plot.png', format='png')
+plt.clf()
+
+#2 column by scatterplot
+plt.scatter(wine['Alcohol'], wine['Malic Acid'], color='b')
+plt.title('Alcohol to Malic Acid')
+plt.xlabel('Malic Acid')
+plt.ylabel('Alcohol')
+plt.savefig(f'plots/Malic Acid_to_Alcohol.png', format='png')
+
+plt.close()
+
+
+
+#print(to_wine)
+#print(wine_DS)
+#print(wine_INFO)
+#print(wine_cor)
---
+++ panadas.py
@@ -0,0 +1,70 @@
+import pandas as pd
+#question number 2
+insurance = pd.read_csv(filepath_or_buffer='insurance.csv', sep=',', header=0)
+open_dataframe = insurance.to_string()
+datacolumn = insurance.columns
+dataindex = insurance.index
+datatypes = insurance.dtypes
+datashape = insurance.shape
+datainfo = insurance.info()
+data_describe = insurance.describe()
+
+#question number 3
+column_age = insurance['age']
+
+#question number 4
+Mutli_column = insurance[['age','children','charges']]
+
+#question number 5
+First_5_age = insurance.iloc[0:5,0:1]
+First_5_children = insurance.iloc[0:5,3:4]
+First_5_charges = insurance.iloc[0:5,6:7]
+
+#question number 6
+charges_average = insurance['charges'].mean()
+charges_min = insurance['charges'].min()
+charges_max = insurance['charges'].max()
+
+#question number 7
+insurance_charge = insurance['charges'] == 10797.3362
+info = insurance[insurance_charge]
+
+#question number 8
+max = insurance['charges'] == charges_max
+max_info = insurance[max]
+
+#question number 9
+region = insurance['region'].value_counts()
+
+#question number 10
+children = insurance['children'].value_counts()
+
+#question number 11
+correlation_four = insurance.corr().to_string()
+
+#question number 12
+method_way = insurance.corr()
+
+
+print(open_dataframe)
+#print(datacolumn)
+#print(dataindex)
+#print(datatypes)
+#print(datashape)
+#print(datainfo)
+#print(data_describe)
+#print(column_age)
+#print(Mutli_column)
+#print(First_5_lines)
+#print(First_5_children)
+#print(First_5_charges)
+#print(charges_average)
+#print(charges_min)
+#print(charges_max)
+#print(info)
+#print(max_info)
+#print(region)
+#print(children)
+#print(correlation_four)
+#print(method_way)
+
